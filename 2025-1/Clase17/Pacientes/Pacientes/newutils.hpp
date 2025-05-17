8b814a428e9524951c57cc3536e9dd37e96834b9
erasmo gomez <hector.gomez@pucp.edu.pe>
2025-05-17 11:12:31 -0500

    update clase17

---
+++ 2025-1/Clase17/Pacientes/Pacientes/newutils.hpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   newutils.hpp
+ * Author: Erasmo Gomez
+ *
+ * Created on May 15, 2025, 5:46 PM
+ */
+
+#ifndef NEWUTILS_HPP
+#define NEWUTILS_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+#define MAX_DATOS 290
+#define NO_ENCONTRADO -1
+using namespace std;
+
+#endif /* NEWUTILS_HPP */
+
