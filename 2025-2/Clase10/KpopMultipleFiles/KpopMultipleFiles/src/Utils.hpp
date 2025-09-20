5783777daca1f9be200b73f817173ad290bbae94
Erasmo <erasmo.gomez@airnguru.com>
2025-09-20 16:11:37 -0500

    update clase 10

---
+++ 2025-2/Clase10/KpopMultipleFiles/KpopMultipleFiles/src/Utils.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Erasmo on 6/09/25.
+//
+
+#ifndef PROFESORES_UTILS_HPP
+#define PROFESORES_UTILS_HPP
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#define ANCHO_REPORTE 140
+#define N_COLUMNS 4
+#define N_COLUMNS_SONGS 7
+#define TITULO_SIZE 20
+#define SUBTITULO_SIZE 35
+using namespace std;
+#endif //PROFESORES_UTILS_HPP
