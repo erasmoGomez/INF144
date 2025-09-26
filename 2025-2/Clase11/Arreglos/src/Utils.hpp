5356e08b567e206f6d64c1dd1fc2e6f0964b07ce
Erasmo <erasmo.gomez@airnguru.com>
2025-09-25 20:49:11 -0500

    update clase 11

---
+++ 2025-2/Clase11/Arreglos/src/Utils.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Erasmo on 25/09/25.
+//
+
+#ifndef ARREGLOS_UTILS_HPP
+#define ARREGLOS_UTILS_HPP
+#include <iostream>
+#include <fstream>
+#define MAX_CODIGOS 10
+using namespace std;
+#endif //ARREGLOS_UTILS_HPP
